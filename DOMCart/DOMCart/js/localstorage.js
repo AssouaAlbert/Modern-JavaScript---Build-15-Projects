00b8883d1832c5fec3bde62888d98750593e8f66
AssouaAlbert <assouaalbert@gmail.com>
2020-01-19 01:46:45 +0300

    Commented old trick of saving one value to local storage.

--- DOMCart/DOMCart/js/localstorage.js
+++ DOMCart/DOMCart/js/localstorage.js
@@ -1,6 +1,7 @@
-//Store content in local Storage
-localStorage.setItem('Name','Henry');
-localStorage.setItem('Name2','Hilary');
-console.log(localStorage.getItem('Name2'));
-localStorage.removeItem('Name:');
-localStorage.clear();
+// //Store content in local Storage
+// localStorage.setItem('Name','Henry');
+// localStorage.setItem('Name2','Hilary');
+// console.log(localStorage.getItem('Name2'));
+// localStorage.removeItem('Name:');
+// localStorage.clear();
+
