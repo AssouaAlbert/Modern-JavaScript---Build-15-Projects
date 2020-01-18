f3e9bdc8b6823ff0d369965defc6673de8438e5b
AssouaAlbert <assouaalbert@gmail.com>
2020-01-19 02:03:50 +0300

    Created and Array anc onverted it to strings using JSON.stringify() function

--- DOMCart/DOMCart/js/localstorage.js
+++ DOMCart/DOMCart/js/localstorage.js
@@ -5,3 +5,20 @@
 // localStorage.removeItem('Name:');
 // localStorage.clear();
 
+const localStorageContent = localStorage.getItem('name');
+//To safe multiple item silmultaneously use an array
+//Only strings can be stored in a local storage meaning tht the array will be converted into a string before it is stored into the local storage
+//Example: Store multiple name in local storage
+let name = [];
+//Name:1
+name.push('Albert');
+name.push('Eloumbat Assoua');
+name.push('Al-Baba');
+//Convert to string
+let stringOfNames = JSON.stringify('name');
+
+console.log = stringOfNames;
+if (localStorageContent === null) {
+
+}
+
