8cc925e9a706f05703680dd2fdb142f68e13fe33
AssouaAlbert <assouaalbert@gmail.com>
2020-01-19 01:45:11 +0300

    localStorage.setItem('Name','Henry'); localStorage.setItem('Name2','Hilary'); console.log(localStorage.getItem('Name2')); localStorage.removeItem('Name:'); localStorage.clear();

--- DOMCart/DOMCart/js/localstorage.js
+++ DOMCart/DOMCart/js/localstorage.js
@@ -1,2 +1,6 @@
 //Store content in local Storage
 localStorage.setItem('Name','Henry');
+localStorage.setItem('Name2','Hilary');
+console.log(localStorage.getItem('Name2'));
+localStorage.removeItem('Name:');
+localStorage.clear();
