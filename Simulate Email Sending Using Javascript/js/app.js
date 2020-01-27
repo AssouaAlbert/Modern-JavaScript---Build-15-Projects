b11c557dae75364b9e11b4be0e2d021866af8ca7
AssouaAlbert <assouaalbert@gmail.com>
2020-01-28 02:41:16 +0300

    Created the constant variables to access all the element which are important in this project

---
+++ Simulate Email Sending Using Javascript/js/app.js
@@ -0,0 +1,15 @@
+//define the Varaibles
+const to = document.getElementById('email'), //Access the To field
+    subject = document.getElemntByID('subject'), //Access the Subject field
+    message = document.getElementById('message'), //Access the Message field
+    form = document.getElementById('email-form'), //Access the Form
+    sendBtn = document.getElementById('sendBtn'), //Access the Send Button
+    reset = document.getElementById('resetBtn'); //Access the To Reset Button
+
+
+//create Event listeners
+    
+
+
+
+//Define the functions
