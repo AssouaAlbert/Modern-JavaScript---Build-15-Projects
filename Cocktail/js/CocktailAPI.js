1547e5400bad6e2a5e0ff378536df899d803c400
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-07-06 09:13:19 +0300

    change http protocols to https

--- Cocktail/js/CocktailAPI.js
+++ Cocktail/js/CocktailAPI.js
@@ -2,7 +2,7 @@ class CocktailAPI {
      // Get recipe by name
      async getDrinksByName(name) {
         // Search by name
-        const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
         // Returns a json respone
         const cocktails = await apiResponse.json();
 
@@ -17,20 +17,20 @@ class CocktailAPI {
      }
      async getSingleRecipe(id){
           // Get a single recipe of a drink
-          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+          const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
           const cocktails = await apiResponse.json();
           return cocktails;
      }
      //Rerieves all the categories from the REST API
      async getCategories(){
-          const apiResponse = await fetch('http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+          const apiResponse = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
           // Wait for response and return JSON
           const categories = await apiResponse.json();
           return categories;
      }
      async getDrinksByCategory(category){
           // Search by Category
-          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+          const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
           // Wait for response then return JSON
           const cocktails = await apiResponse.json();
           return cocktails
